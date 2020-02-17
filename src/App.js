f94d5fd43a098d8576a7672bce422bec0305b1df
Vasilii Komarov <komarov.vasiliy884@gmail.com>
2020-02-17 22:51:28 +0400

    Some changes have been made

--- src/App.js
+++ src/App.js
@@ -4,22 +4,53 @@ import IrregularVerb from "./components/IrregularVerb"
 
 class App extends React.Component {
 
+  state = {
+    infinitive: '',
+    infinitiveTranscription: '',
+    pastSimple: '',
+    pastSimpleTranscription: '',
+    pastPrinciple: '',
+    pastPrincipleTranscription: '',
+    meaning: '',
+    error: ''
+  }
+
   getVerb = async (event) => {
     event.preventDefault();
-    const data = await fetch("http://localhost:8080/api/v0.1/irregular-verbs");
+    const data = await fetch("http://localhost:8080/api/v0.1/irregular-verbs/random");
     console.log(data);
     const dataJson = await data.json();
     console.log(dataJson);
+
+    this.setState({
+      infinitive: dataJson.infinitive,
+      infinitiveTranscription: dataJson.infinitiveTranscription,
+      pastSimple: dataJson.pastSimple,
+      pastSimpleTranscription: dataJson.pastSimpleTranscription,
+      pastPrinciple: dataJson.pastPrinciple,
+      pastPrincipleTranscription: dataJson.pastPrincipleTranscription,
+      meaning: dataJson.meaning,
+      error: ""
+    });
   }
 
   render() {
-    return(
+    return (
       <div>
         <h2>Vocabulator</h2>
         <p><a href="/irregular-verbs">Irregular verbs</a></p>
         <p>komarov884</p>
-        <p>2019</p>
-        <IrregularVerb getVerb={this.getVerb} />
+        <p>2020</p>
+        <IrregularVerb
+          getVerb={this.getVerb}
+          infinitive={this.state.infinitive}
+          infinitiveTranscription={this.state.infinitiveTranscription}
+          pastSimple={this.state.pastSimple}
+          pastSimpleTranscription={this.state.pastSimpleTranscription}
+          pastPrinciple={this.state.pastPrinciple}
+          pastPrincipleTranscription={this.state.pastPrincipleTranscription}
+          meaning={this.state.meaning}
+        />
       </div>
     );
   }
--- src/components/IrregularVerb.js
+++ src/components/IrregularVerb.js
@@ -1,17 +1,34 @@
 import React from "react";
 
 class IrregularVerb extends React.Component {
+
+  state = {
+    pastSimple: '',
+    pastPrinciple: ''
+  }
+
+  handleInputChange(event) {
+    this.setState({
+      pastSimple: this.inputPastSimple.value,
+      pastPrinciple: this.inputPastPrinciple.value
+    })
+  }
+
+  check(pastSimple, pastPrinciple) {
+    {/* TODO imolement */}
+  }
+
   render() {
     return(
       <div>
         <h2>Irregular verbs</h2>
-        <div><p>Meaning</p><p>verb.meaning</p></div>
-        <div><p>Infinitive</p><input id="inputInfinitive" value="verb.infinitive"/></div>
-        <div><p>Past simple</p><input id="inputPastSimple" value="verb.pastSimple"/></div>
-        <div><p>Past principle</p><input id="inputPastPrinciple" value="verb.pastPrinciple"/></div>
+        <div><p>Meaning</p><p>{this.props.meaning}</p></div>
+        <div><p>Infinitive</p><input id="inputInfinitive" readOnly value={this.props.infinitive}/></div>
+        <div><p>Past simple</p><input id="inputPastSimple" value={this.props.pastSimple} onChange={() => this.handleInputChange}/></div>
+        <div><p>Past principle</p><input id="inputPastPrinciple" value={this.props.pastPrinciple} onChange={() => this.handleInputChange}/></div>
         <p>komarov884</p>
-        {/*<button id="check" onClick="check(inputInfinitive.value, inputInfinitive.value)">Check</button>*/}
         <button id="getVerb" onClick={this.props.getVerb}>getVerb</button>
+        <button id="check" onClick={() => this.check(() => this.inputPastSimple.value, () => this.inputPastPrinciple.value)}>Check</button>
         <div><p>Result</p><label id="result">result</label></div>
       </div>
     );
